b19bb808b6716a02a1567fb70f1703abefdee17d
aszokalski <szokalskiadam@gmail.com>
2020-11-13 02:51:27 +0100

    a

--- beta.php
+++ beta.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "rohlldln_beta";
 $password = "Dupa1234";
 $dbname = "rohlldln_beta";
-
+echo "dupa";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -18,6 +18,10 @@ $q1 = $_POST["q1"];
 $q2 = $_POST["q2"];
 $q3 = $_POST["q3"];
 $tn = $_POST["inlineRadioOptions"];
+
+echo $q3;
+echo $tn;
+
 
 $sql = "INSERT INTO Users (Emai, Zawod, Tel, Odp1, Odp2, Odp3, Odp4)
 VALUES ($email, $job, $phone, $tn, $q1, $q2, $q3)";
